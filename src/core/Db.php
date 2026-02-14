ad6cb363b5f01ace826d9d7b6877628764454db0
Mertens Adrien <mertensadrien23@hotmail.be>
2026-02-14 08:07:32 +0100

    ajout de l'environement et modif du gitignore

--- src/core/Db.php
+++ src/core/Db.php
@@ -5,25 +5,43 @@ namespace App\Core;
 use PDO;
 use PDOException;
 
+// Charger les variables d'environnement depuis le fichier .env
+$env = parse_ini_file(__DIR__ . '/../../.env') ?: [];
+
 class Db extends PDO
 {
     // Instance unique de la classe
     private static $instance;
+    
 
-    // Informations de connexion
-    private const DBHOST = 'localhost';
-    private const DBUSER = 'root';
-    private const DBPASS = '';
-    private const DBNAME = 'examenphp';
+    // Informations de connexion chargées depuis le fichier .env avec valeurs par défaut
+    private static $dbhost;
+    private static $dbuser;
+    private static $dbpass;
+    private static $dbname;
+    
+    /**
+     * Charge les paramètres de connexion depuis le fichier .env
+     *
+     * @return void
+     */
+    private static function loadConfig(): void
+    {
+        global $env;
+        self::$dbhost = $env['DB_HOST'] ?? 'localhost';
+        self::$dbuser = $env['DB_USER'] ?? 'root';
+        self::$dbpass = $env['DB_PASS'] ?? '';
+        self::$dbname = $env['DB_NAME'] ?? 'examenphp';
+    }
 
     private function __construct()
     {
         // DSN de connexion
-        $_dsn = 'mysql:dbname=' . self::DBNAME . ';host=' . self::DBHOST;
+        $_dsn = 'mysql:dbname=' . self::$dbname . ';host=' . self::$dbhost;
 
         // On appelle le constructeur de la classe PDO
         try {
-            parent::__construct($_dsn, self::DBUSER, self::DBPASS);
+            parent::__construct($_dsn, self::$dbuser, self::$dbpass);
 
             $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
             $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); // On veut des objets, pas des tableaux
@@ -37,6 +55,7 @@ class Db extends PDO
     public static function getInstance(): self
     {
         if(self::$instance === null){
+            self::loadConfig();
             self::$instance = new self();
         }
         return self::$instance;
